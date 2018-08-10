4578e3eb7a6e33029ce3f62cddb111be1e9a41c6
adx59 <adamxu59@mgmail.com>
2018-08-10 16:42:36 -0700

    definition command

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import googletrans
+import PyDictionary
 
 from discord.ext import commands
 import discord
@@ -7,6 +8,7 @@ import discord
 class Utility(object):
     def __init__(self):
         self.translator = googletrans.Translator()
+        self.dictionary = PyDictionary.PyDictionary()
 
     @commands.command(name="translate_en", aliases=["tren"])
     async def _translate_en(self, ctx, *, text: str):
@@ -30,6 +32,24 @@ class Utility(object):
             f"```{translated}```")
         )
 
+    @commands.command(name="define", aliases=["def"])
+    async def _define(self, ctx, *, term: str):
+        """Defines a word."""
+        formatted = f"**Definition of `{term}`**\n"
+
+        async with ctx.typing():
+            definition = self.dictionary.meaning(term)
+            if definition is None:
+                await ctx.send(":no_entry_sign: No definition found for that term.")
+                return
+
+        for t in definition:
+            formatted += f"{t}:\n```css\n"
+            for subdef in definition[t]:
+                formatted += f"- {subdef}"
+            formatted += "\n```\n"  
+
+        await ctx.send(formatted)
     
 def setup(bot):
     bot.add_cog(Utility())
