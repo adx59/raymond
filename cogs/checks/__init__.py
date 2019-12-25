3dd15c36786656217a499cdcbe6c1832bddd62ee
adx59 <adamxu59@gmail.com>
2019-12-25 13:27:44 -0800

    unfuck bot, add config.example

--- cogs/checks/__init__.py
+++ cogs/checks/__init__.py
@@ -3,6 +3,5 @@ from discord.ext import commands
 
 def is_dev():
         def predicate(ctx):
-            print(ctx.bot.config)
             return ctx.author.id in ctx.bot.config['owners']
         return commands.check(predicate)
